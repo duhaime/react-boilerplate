cfb4a6ead197590834eb3887fdf78de81df64737
duhaime <douglas.duhaime@gmail.com>
2016-12-11 21:40:40 -0500

    simplify webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,141 @@
 // webpack.config.js
-var StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin')
-var data = require('./data')
+var StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var webpack = require('webpack');
+var merge = require('webpack-merge');
+var path = require('path');
+var data = require('./data');
 
-module.exports = {
-  entry: './src/Index',
+var TARGET = process.env.npm_lifecycle_event;
 
+var PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build'),
+  node_modules: path.join(__dirname, 'node_modules')
+};
+
+// Define configuration options common to
+// development and production environments
+var common = {
+
+  entry: {
+    app: PATHS.app + "/Index"
+  },
+
+  // Use the history api fallback so React routes
+  // are obeyed
+  devServer: {
+    historyApiFallback: true
+  },
+
+  // Specify which assets webpack should load
+  // '' allows files without an extension
+  // to be loaded
   resolve: {
     extensions: ['', '.js', '.jsx']
   },
 
+  // Specify where compiled assets will be bundled
   output: {
+    path: PATHS.build,
     filename: 'bundle.js',
-    path: 'build',
     libraryTarget: 'umd'
   },
 
+  // Include loaders for styles and jsx
+  // the include param specifies the location
+  // of files to be loaded (a subdomain of PATHS)
   module: {
     loaders: [
       {
-        test: /\.js/,
-        loader: 'babel',
-        include: __dirname + '/src',
+        test: /\.css$/,
+        loaders: ['style', 'css'],
+        include: [PATHS.app, PATHS.node_modules]
       },
-
       {
-        test: /\.jsx$/,
-        loaders: ['jsx-loader', 'babel?cacheDirectory']
+        test: /\.jsx?$/,
+        // Leverage caching for better performance
+        loaders: ['babel?cacheDirectory'],
+        include: PATHS.app
       }
     ]
-  },
-
-  plugins: [
-    new StaticSiteGeneratorPlugin('bundle.js', data.routes, data)
-  ]
+  }
 }
+
+// Development configuration
+if(TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+
+    // Enable sourcemaps for debugging
+    devtool: 'eval-source-map',
+
+    // Configure server
+    devServer: {
+      contentBase: PATHS.build,
+
+      // Enable history API fallback to facilitate
+      // API-based routing.
+      historyAPIFallback: true,
+      hot: true,
+      inline: true,
+      progress: true,
+
+      // Display only errors to minimize output
+      stats: 'errors-only',
+
+      // When using Vagrant or other VM, set:
+      // host: process.env.HOST || '0.0.0.0';
+      //
+      // 0.0.0.0 is available to all network devices
+      // unlike default
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+
+      // Use hot module replacement
+      new webpack.HotModuleReplacementPlugin()
+    ]
+  });
+}
+
+// Production configuration
+if(TARGET === 'build' || !TARGET) {
+  module.exports = merge(common, {
+
+    entry: PATHS.app + '/index',
+    output: {
+      path: PATHS.build,
+      filename: 'bundle.js',
+      libraryTarget: 'umd'
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.js/,
+          loader: 'babel',
+          include: PATHS.app
+        },
+        {
+          test: /\.css$/,
+          loaders: ['style', 'css'],
+          include: [PATHS.app, PATHS.node_modules]
+        },
+        {
+          test: /\.(jpg|png)/,
+          loader: 'file-loader?name=assets/img-[hash:6].[ext]',
+          include: PATHS.app
+        }
+      ],
+    },
+    plugins: [
+      new ExtractTextPlugin("styles.css"),
+      new StaticSiteGeneratorPlugin('main', data.routes, data),
+      new webpack.DefinePlugin({
+        'process.env': {
+          'NODE_ENV': JSON.stringify('production')
+        }
+      })
+    ]
+  })
+};
